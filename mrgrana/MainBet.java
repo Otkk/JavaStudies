4db37a562042948a60ca3726f475d5abf7ea76cd
José Guilherme <119455751+Otkk@users.noreply.github.com>
2025-09-25 20:37:42 -0300

    Update MainBet.java

--- mrgrana/MainBet.java
+++ mrgrana/MainBet.java
@@ -2,13 +2,13 @@ package mrgrana;
 
 public class MainBet {
     public static void main(String[] args) {
-        // 1. Cria a corrida primeiro
+        
 
         Corrida corrida1 = new Corrida();
-        Aposta aposta1 = new Aposta(500.0, "Cavalo 2", corrida1.getIdCorrida());
+        Aposta aposta1 = new Aposta(500.0, 2);
         System.out.println("Aposta criada:");
 
-
+       
         System.out.println("Corrida " + corrida1.getIdCorrida() + " finalizada!");
         System.out.println("Resultados: " + corrida1.getResultados());
         System.out.println("Vencedor da corrida: " + corrida1.getCavaloVencedor());
@@ -16,14 +16,14 @@ public class MainBet {
 
 
         System.out.println("ID da Aposta: " + aposta1.getIdAposta());
-        System.out.println("ID da Corrida: " + aposta1.getIdCorrida());
+        System.out.println("ID da Corrida: " + aposta1.getCorrida().getIdCorrida());
         System.out.println("Valor apostado: " + aposta1.getValorAposta());
         System.out.println("Palpite: " + aposta1.getPalpiteCavalo());
 
-        if (aposta1.getPalpiteCavalo().equals(corrida1.getCavaloVencedor())) {
-            System.out.println("Parabéns, você acertou o vencedor da corrida " + corrida1.getIdCorrida() + "!");
+        if (aposta1.getPalpiteCavalo() == (corrida1.getCavaloVencedor())) {
+            System.out.println("Parabéns, você acertou o cavalo vencedor da corrida " + corrida1.getIdCorrida() + "!");
         } else {
-            System.out.println("Que pena, você errou. O vencedor foi o " + corrida1.getCavaloVencedor() + ".");
+            System.out.println("Que pena, você errou. O vencedor foi o cavalo " + corrida1.getCavaloVencedor() + ".");
         }
     }
 }
